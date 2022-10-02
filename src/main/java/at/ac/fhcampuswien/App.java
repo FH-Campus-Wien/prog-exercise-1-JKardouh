9bd328d87dc4e6651a7e33a87462e6c0500f7631
Jkardouh <judy.kardouh@stud.fh-campuswien.ac.at>
2022-10-02 11:31:35 +0200

    Solved

--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -6,52 +6,131 @@ public class App {
 
     //todo Task 1
     public void sayHelloWorld(){
-        // input your solution here
+        System.out.println("Hello World!");
     }
 
     //todo Task 2
     public void helloRobot(){
-        // input your solution here
+        System.out.println("0123456789012345678901");
+        System.out.println("         __");
+        System.out.println(" _(\\    |@@|");
+        System.out.println("(__/\\__ \\--/ __");
+        System.out.println("   \\___|----|  |   __");
+        System.out.println("       \\ }{ /\\ )_ / _\\");
+        System.out.println("       /\\__/\\ \\__O (__");
+        System.out.println("      (--/\\--)    \\__/");
+        System.out.println("      _)(  )(_");
+        System.out.println("     `---''---`");
     }
 
     //todo Task 3
     public void sumOfLiterals(){
-        // input your solution here
+        char a  ='Z';
+        int b = 0xface;
+        int c = 012;
+        long d = 80L;
+        float e = 44e-1f;
+        float f = 5.5f;
+        double g = 8.88e1;
+        double h = 99.9;
+
+        System.out.println((int) a + b + c + (int) d + (int) e + (int) f + (int) g + (int) h);
     }
 
     //todo Task 4
     public void addTwoNumbers(){
-        // input your solution here
+        Scanner scanner = new Scanner(System.in);
+        int numA = scanner.nextInt();
+        int numB = scanner.nextInt();
+        int result =  numA + numB;
+        System.out.println(result);
     }
 
     //todo Task 5
     public void swapTwoNumbers(){
-        // input your solution here
+        Scanner scanner = new Scanner(System.in);
+        int oldA = scanner.nextInt(); //5
+        int oldB = scanner.nextInt(); //7
+        int difference = oldA - oldB;
+        int newA = oldA - difference;
+        int newB = oldB + difference;
+        //corner case: if difference is negative we can multiply with -1 so its positive again
+        System.out.println("Before Swap:");
+        System.out.print("x: ");
+        System.out.print("y: ");
+        System.out.println("After Swap:");
+        System.out.println("x: "+ newA);
+        System.out.println("y: "+ newB);
     }
 
     //todo Task 6
     public void compareTwoNumbers(){
-        // input your solution here
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("n1: ");
+        int num1 = scanner.nextInt();
+        System.out.print("n2: ");
+        int num2 = scanner.nextInt();
+
+        if (num1 > num2) System.out.println("n1 > n2");
+        if(num1 < num2) System.out.println("n2 > n1");
+        if(num1 == num2) System.out.println("n1 == n2");
     }
 
     //todo Task 7
     public void ratingSalesPerson(){
-        // input your solution here
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter annual Revenue: ");
+        int revenue = scanner.nextInt();
+        if(revenue < 0 || revenue >= 100000) System.out.println("Invalid Revenue");
+        else if (revenue < 20000) System.out.println("Poor Sales Revenue");
+        else if (revenue < 50000) System.out.println("Average Sales Revenue");
+        else if (revenue < 80000) System.out.println("Good Sales Revenue");
+        else System.out.println("Excellent Sales Revenue");
     }
 
     //todo Task 8
     public void getCommissionRate(){
-        // input your solution here
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter CommissionClass: ");
+        double commission;
+        int commissionClass = scanner.nextInt();
+
+        switch (commissionClass){
+            case 1: commission = 0.01; break;
+            case 2: commission = 0.02; break;
+            case 3: commission = 0.03; break;
+            case 4: commission = 0.04; break;
+            default: commission = 0.00;
+        }
+        System.out.println("Your Commission Rate was set to " + commission);
+
     }
 
     //todo Task 9
     public void leapyear(){
-        // input your solution here
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Year: ");
+        int year = scanner.nextInt();
+        if (year % 4 == 0 && !(year % 100 == 0) || year % 400 == 0) {
+            System.out.println("Leapyear");
+        }
+        else System.out.println("Not a Leapyear");
     }
 
     //todo Task 10
     public void transposedNumbers(){
-        // input your solution here
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Number: ");
+        int number = scanner.nextInt();
+        int reverse = 0;
+        while (number!= 0){
+            int rest = number %10;
+            reverse = (reverse * 10) + rest;
+            number = number/10;
+        }
+
+        System.out.println(reverse);
     }
 
 
@@ -59,25 +138,25 @@ public class App {
         App exercise1 = new App();
 
         System.out.println("Task 1: Say Hello World");
-        exercise1.sayHelloWorld();
+        //exercise1.sayHelloWorld();
 
         System.out.println("\nTask 2: Hello Robot");
-        exercise1.helloRobot();
+        //exercise1.helloRobot();
 
         System.out.println("\nTask 3: Literals");
-        exercise1.sumOfLiterals();
+        //exercise1.sumOfLiterals();
 
         System.out.println("\nTask 4: Add two Numbers");
-        exercise1.addTwoNumbers();
+        //exercise1.addTwoNumbers();
 
         System.out.println("\nTask 5: Swap two Numbers without using a helper variable");
-        exercise1.swapTwoNumbers();
+        //exercise1.swapTwoNumbers();
 
         System.out.println("\nTask 6: Compare two Numbers");
-        exercise1.compareTwoNumbers();
+        //exercise1.compareTwoNumbers();
 
         System.out.println("\nTask 7: Rating Sales Person");
-        exercise1.ratingSalesPerson();
+        //exercise1.ratingSalesPerson();
 
         System.out.println("\nTask 8: Commission Rate");
         exercise1.getCommissionRate();
